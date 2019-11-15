a63e1de2b755a67f50d440792e7a696477bec6ce
Ahmed Kassem <ahmedkassem996@gmail.com>
2019-11-16 01:56:36 +0200

    Initial Commit

---
+++ Remind/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  Remind
+//
+//  Created by AHMED on 8/25/1398 AP.
+//  Copyright © 1398 AHMED. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
